8a5c7ec6ab428540b63759b378101335e704572c
JonnyAsher <JonnyAsher@users.noreply.github.com>
2017-10-29 19:37:04 -0400

    law script

---
+++ Scripts/1.3-ja-lawlargenumbers.R
@@ -0,0 +1,19 @@
+
+N <-50000
+counter <- 0
+for(bitch in rnorm(N, mean = 69, sd=1)){
+  print(bitch)
+  if( (bitch > 68) & (bitch < 70) ){
+    counter = counter + 1
+  }
+}
+counter / N 
+rnorm(5000) -> jew
+colors <- c(
+  "blue",
+  "firebrick",
+  "forestgreen"
+)
+hist(jew, col = colors, main = "JEW")
+
+?hist
